f13172313d3e7d84c54156c9e74702cfffc617c6
jarrelp <jarrel.pothoff@gmail.com>
2022-10-20 01:14:21 +0200

    apexraderchart skeleton resize (grove) fix
    Deze fix kan op nettere manieren

--- src/ui-component/cards/skeleton/ApexRadarChart.js
+++ src/ui-component/cards/skeleton/ApexRadarChart.js
@@ -1,31 +1,36 @@
 // material-ui
 import { Card, CardContent, Grid } from '@mui/material';
 import Skeleton from '@mui/material/Skeleton';
+import UseWindowSize from 'hooks/useWindowSize';
 
 // project imports
 import { gridSpacing } from 'store/constant';
 
 // ==============================|| SKELETON TOTAL GROWTH BAR CHART ||============================== //
 
-const ApexRadarChart = () => (
-    <Card>
-        <CardContent>
-            <Grid container spacing={gridSpacing}>
-                <Grid item xs={12}>
-                    <Grid container alignItems="center" justifyContent="space-between" spacing={gridSpacing}>
-                        <Grid item xs zeroMinWidth>
-                            <Grid item xs={1}>
-                                <Skeleton variant="text" height={30} />
+const ApexRadarChart = () => {
+    const [widtht] = UseWindowSize();
+
+    return (
+        <Card>
+            <CardContent>
+                <Grid container spacing={gridSpacing}>
+                    <Grid item xs={12}>
+                        <Grid container alignItems="center" justifyContent="space-between" spacing={gridSpacing}>
+                            <Grid item xs zeroMinWidth>
+                                <Grid item xs={1}>
+                                    <Skeleton variant="text" height={30} />
+                                </Grid>
                             </Grid>
                         </Grid>
                     </Grid>
+                    <Grid item xs={12} height={widtht - widtht * 0.2}>
+                        <Skeleton variant="rectangular" height="100%" />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sx={{ p: 2, border: '1px dashed grey' }}>
-                    <Skeleton variant="rectangular" />
-                </Grid>
-            </Grid>
-        </CardContent>
-    </Card>
-);
+            </CardContent>
+        </Card>
+    );
+};
 
 export default ApexRadarChart;
