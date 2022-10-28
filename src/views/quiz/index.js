177640f3f03c3650db689a7d4bbf16fc24c65672
jarrelp <jarrel.pothoff@gmail.com>
2022-10-28 15:20:51 +0200

    use radio

--- src/views/quiz/index.js
+++ src/views/quiz/index.js
@@ -1,15 +1,18 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import { Link } from 'react-router-dom';
 
 // material-ui
-import { Button, CardActions, Grid, Typography } from '@mui/material';
+import { Button, CardActions, Grid, Typography, FormControl, FormControlLabel, Radio, RadioGroup } from '@mui/material';
 
 // project imports
 import UIProgress from 'ui-component/UIProgress';
 import AnimateButton from 'ui-component/extended/AnimateButton';
-import Question from './question';
+import MainCard from 'ui-component/cards/MainCard';
+import SubCard from 'ui-component/cards/SubCard';
 import QuizData from './quiz-data';
+
+import { gridSpacing } from 'store/constant';
 
 // // quizpanel
 // function QuizPanel({ children, value, index, ...other }) {
@@ -48,11 +51,11 @@ import QuizData from './quiz-data';
 
 // ==============================|| QUIZ ||============================== //
 
-const userAnswersInitialState = () => {
-    var array = new Array();
-    QuizData.map((item) => array.push(item.id));
-    return array;
-};
+// const userAnswersInitialState = () => {
+//     var array = new Array();
+//     QuizData.map((item) => array.push(item.id));
+//     return array;
+// };
 
 const Quiz = () => {
     const [number, setNumber] = useState(0);
@@ -60,62 +63,138 @@ const Quiz = () => {
     // matrix
     // first row: question
     // seccond row: answer
-    const [userAnswers, setUserAnswers] = useState(new Array(Object.keys(QuizData).length)); //new Array(Object.keys(QuizData).length)
+    // const [userAnswers, setUserAnswers] = useState(new Array(Object.keys(QuizData).length)); //new Array(Object.keys(QuizData).length)
 
-    const setUserAnswer = (questionId, userAnswerId) => {
-        var data = userAnswersInitialState;
-        var index = data.indexOf(questionId);
-        var array = userAnswers;
-        array[index] = userAnswerId;
-        setUserAnswers(array);
-    };
+    // const setUserAnswer = (questionId, userAnswerId) => {
+    //     var data = userAnswersInitialState;
+    //     var index = data.indexOf(questionId);
+    //     var array = userAnswers;
+    //     array[index] = userAnswerId;
+    //     setUserAnswers(array);
+    // };
 
     // const handleChange = (event, questionId, newNumber) => {
     //     var index = userAnswers.findIndex(questionId);
     //     userAnswers.
     // };
 
+    // stappenplan
+    // nog steeds: maak 2 dimentionale list aan die de aangeklikte antwoord per vraag onthoudt.
+
+    //radio buttons
+    // const [valueBasic, setValueBasic] = useState([]); // dit moet veranderd worden
+    const [answer, setAnswer] = useState({});
+
+    // const setValueBasicEvent = (event, questionKey) => {
+    //     var array = valueBasic;
+    //     array[questionKey] = event.target.value;
+    //     setValueBasic(array);
+    // };
+
     const handleChange = (event, newNumber) => {
         setNumber(newNumber);
     };
 
+    // const showAnswers = () => {
+    //     valueBasic.map((answer, key) => console.log(answer[key] + '\n'));
+    // };
+
+    // useEffect(() => {
+    //     console.log('rerender');
+    // }, valueBasic);
+
     return (
         <>
-            <Question question={QuizData[number]} setUserAnswer={setUserAnswer}>
-                <UIProgress value={number + 1} maxValue={Object.keys(QuizData).length - 1} />
-                <CardActions>
-                    <Grid container justifyContent="space-between" spacing={0}>
-                        <Grid item>
-                            {number > 0 && (
-                                <AnimateButton>
-                                    <Button variant="outlined" size="large" onClick={(e) => handleChange(e, number - 1)}>
-                                        Previous
-                                    </Button>
-                                </AnimateButton>
-                            )}
-                        </Grid>
-                        <Grid item>
-                            {number < Object.keys(QuizData).length - 1 && (
-                                <AnimateButton>
-                                    <Button variant="contained" size="large" onClick={(e) => handleChange(e, 1 + number)}>
-                                        Next
-                                    </Button>
-                                </AnimateButton>
-                            )}
-                            {number === Object.keys(QuizData).length - 1 && (
-                                <AnimateButton>
-                                    <Button variant="contained" size="large" component={Link} to="/result">
-                                        Result
-                                    </Button>
-                                </AnimateButton>
-                            )}
+            {QuizData.map((question, questionKey) => (
+                <MainCard title={question.description} key={questionKey} content={true}>
+                    <Grid container spacing={gridSpacing}>
+                        {question.answers?.map((options, answerKey) => (
+                            <Grid item xs={12} lg={12} key={answerKey}>
+                                <SubCard>
+                                    <Grid container spacing={2}>
+                                        <Grid item sm={2} md={1} lg={1} xl={1} xxl={1}>
+                                            {/* <FormControl> */}
+                                            <Radio
+                                                defaultValue={question.answers[0].id}
+                                                aria-label={question.id}
+                                                value={options.id}
+                                                onChange={
+                                                    // (e) => setValueBasicEvent(e, questionKey)
+
+                                                    // setValueBasic((answer.id) => ({
+                                                    //     ...valueBasic,
+                                                    //     [question.id]
+                                                    // }))
+
+                                                    () =>
+                                                        setAnswer((answer) => ({
+                                                            ...answer,
+                                                            [question.id]: options.id
+                                                        }))
+                                                }
+                                                checked={
+                                                    // valueBasic[questionKey] === answer.id
+                                                    answer[question.id] === options.id
+                                                }
+                                                name={question.id}
+                                            >
+                                                {/* <FormControlLabel value={answer.id} control={<Radio />} label={answer.place} /> */}
+                                            </Radio>
+                                            {/* </FormControl> */}
+                                        </Grid>
+                                        <Grid item xs={10}>
+                                            <Typography>{answer.description}</Typography>
+                                        </Grid>
+                                    </Grid>
+                                </SubCard>
+                            </Grid>
+                        ))}
+                        <Grid item xs={12} lg={12}>
+                            <UIProgress value={number + 1} maxValue={Object.keys(QuizData).length - 1} />
+                            <CardActions>
+                                <Grid container justifyContent="space-between" spacing={0}>
+                                    <Grid item>
+                                        {number > 0 && (
+                                            <AnimateButton>
+                                                <Button variant="outlined" size="large" onClick={(e) => handleChange(e, number - 1)}>
+                                                    Previous
+                                                </Button>
+                                            </AnimateButton>
+                                        )}
+                                    </Grid>
+                                    <Grid item>
+                                        {number < Object.keys(QuizData).length - 1 && (
+                                            <AnimateButton>
+                                                <Button variant="contained" size="large" onClick={(e) => handleChange(e, 1 + number)}>
+                                                    Next
+                                                </Button>
+                                            </AnimateButton>
+                                        )}
+                                        {number === Object.keys(QuizData).length - 1 && (
+                                            <AnimateButton>
+                                                <Button
+                                                    variant="contained"
+                                                    size="large"
+                                                    // onClick={() => showAnswers}
+                                                    component={Button}
+                                                    // component={Link}
+                                                    // to="/result"
+                                                >
+                                                    Result
+                                                </Button>
+                                            </AnimateButton>
+                                        )}
+                                    </Grid>
+                                </Grid>
+                            </CardActions>
+                            <Typography textAlign="center">
+                                Question: {number + 1} of {Object.keys(QuizData).length}
+                            </Typography>
                         </Grid>
                     </Grid>
-                </CardActions>
-                <Typography textAlign="center">
-                    Question: {number + 1} of {Object.keys(QuizData).length}
-                </Typography>
-            </Question>
+                </MainCard>
+            ))}
+            {/* {console.log(valueBasic)} */}
         </>
     );
 };
--- src/views/quiz/quiz-data/index.js
+++ src/views/quiz/quiz-data/index.js
@@ -1,251 +1,251 @@
 const QuizData = [
     {
-        id: 'question1',
+        id: '1',
         description: 'question1',
         answers: [
             {
-                id: 'question1answer1',
+                id: '1',
                 description: 'question1answer1',
                 place: 'A'
             },
             {
-                id: 'question1answer2',
+                id: '2',
                 description: 'question1answer2',
                 place: 'B'
             },
             {
-                id: 'question1answer3',
+                id: '3',
                 description: 'question1answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question2',
+        id: '2',
         description: 'question2',
         answers: [
             {
-                id: 'question2answer1',
+                id: '1',
                 description: 'question2answer1',
                 place: 'A'
             },
             {
-                id: 'question2answer2',
+                id: '2',
                 description: 'question2answer2',
                 place: 'B'
             },
             {
-                id: 'question2answer3',
+                id: '3',
                 description: 'question2answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question3',
+        id: '3',
         description: 'question3',
         answers: [
             {
-                id: 'question3answer1',
+                id: '1',
                 description: 'question3answer1',
                 place: 'A'
             },
             {
-                id: 'question3answer2',
+                id: '2',
                 description: 'question3answer2',
                 place: 'B'
             },
             {
-                id: 'question3answer3',
+                id: '3',
                 description: 'question3answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question4',
+        id: '4',
         description: 'question4',
         answers: [
             {
-                id: 'question4answer1',
+                id: '1',
                 description: 'question4answer1',
                 place: 'A'
             },
             {
-                id: 'question4answer2',
+                id: '2',
                 description: 'question4answer2',
                 place: 'B'
             },
             {
-                id: 'question4answer3',
+                id: '3',
                 description: 'question4answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question5',
+        id: '5',
         description: 'question5',
         answers: [
             {
-                id: 'question5answer1',
+                id: '1',
                 description: 'question5answer1',
                 place: 'A'
             },
             {
-                id: 'question5answer2',
+                id: '2',
                 description: 'question5answer2',
                 place: 'B'
             },
             {
-                id: 'question5answer3',
+                id: '3',
                 description: 'question5answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question6',
+        id: '6',
         description: 'question6',
         answers: [
             {
-                id: 'question6answer1',
+                id: '1',
                 description: 'question6answer1',
                 place: 'A'
             },
             {
-                id: 'question6answer2',
+                id: '2',
                 description: 'question6answer2',
                 place: 'B'
             },
             {
-                id: 'question6answer3',
+                id: '3',
                 description: 'question6answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question7',
+        id: '7',
         description: 'question7',
         answers: [
             {
-                id: 'question7answer1',
+                id: '1',
                 description: 'question7answer1',
                 place: 'A'
             },
             {
-                id: 'question7answer2',
+                id: '2',
                 description: 'question7answer2',
                 place: 'B'
             },
             {
-                id: 'question7answer3',
+                id: '3',
                 description: 'question7answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question8',
+        id: '8',
         description: 'question8',
         answers: [
             {
-                id: 'question8answer1',
+                id: '1',
                 description: 'question8answer1',
                 place: 'A'
             },
             {
-                id: 'question8answer2',
+                id: '2',
                 description: 'question8answer2',
                 place: 'B'
             },
             {
-                id: 'question8answer3',
+                id: '3',
                 description: 'question8answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question9',
+        id: '9',
         description: 'question9',
         answers: [
             {
-                id: 'question9answer1',
+                id: '1',
                 description: 'question9answer1',
                 place: 'A'
             },
             {
-                id: 'question9answer2',
+                id: '2',
                 description: 'question9answer2',
                 place: 'B'
             },
             {
-                id: 'question9answer3',
+                id: '3',
                 description: 'question9answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question10',
+        id: '10',
         description: 'question10',
         answers: [
             {
-                id: 'question10answer1',
+                id: '1',
                 description: 'question10answer1',
                 place: 'A'
             },
             {
-                id: 'question10answer2',
+                id: '2',
                 description: 'question10answer2',
                 place: 'B'
             },
             {
-                id: 'question10answer3',
+                id: '3',
                 description: 'question10answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question11',
+        id: '11',
         description: 'question11',
         answers: [
             {
-                id: 'question11answer1',
+                id: '1',
                 description: 'question11answer1',
                 place: 'A'
             },
             {
-                id: 'question11answer2',
+                id: '2',
                 description: 'question11answer2',
                 place: 'B'
             },
             {
-                id: 'question11answer3',
+                id: '3',
                 description: 'question11answer3',
                 place: 'C'
             }
         ]
     },
     {
-        id: 'question12',
+        id: '12',
         description: 'question12',
         answers: [
             {
-                id: 'question12answer1',
+                id: '1',
                 description: 'question12answer1',
                 place: 'A'
             },
             {
-                id: 'question12answer2',
+                id: '2',
                 description: 'question12answer2',
                 place: 'B'
             },
             {
-                id: 'question12answer3',
+                id: '3',
                 description: 'question12answer3',
                 place: 'C'
             }
