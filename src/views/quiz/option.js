c67429c0380df903249021438cf803fc7d3152d7
jarrelp <jarrel.pothoff@gmail.com>
2022-10-30 17:18:16 +0100

    code cleanup and add onclick div

--- src/views/quiz/option.js
+++ src/views/quiz/option.js
@@ -8,31 +8,35 @@ import SubCard from 'ui-component/cards/SubCard';
 // ==============================|| OPTION ||============================== //
 
 const Option = (props) => {
-    var list = props.optionList;
+    let list = props.optionList;
+
+    const handleChange = () => {
+        props.setOptionOnChange((list) => ({
+            ...list,
+            [props.questionId]: props.option.id
+        }));
+    };
 
     return (
-        <SubCard>
-            <Grid container spacing={2}>
-                <Grid item sm={2} md={1} lg={1} xl={1} xxl={1}>
-                    <Radio
-                        aria-label={props.questionId}
-                        value={props.option.id}
-                        onChange={() =>
-                            props.setOptionOnChange((list) => ({
-                                ...list,
-                                [props.questionId]: props.option.id
-                            }))
-                        }
-                        checked={list[props.questionId] === props.option.id}
-                        name={props.questionId}
-                    />
-                    <label>{props.option.place}</label>
+        <div onClick={handleChange}>
+            <SubCard>
+                <Grid container spacing={2}>
+                    <Grid item sm={2} md={1} lg={1} xl={1} xxl={1}>
+                        <Radio
+                            aria-label={props.questionId}
+                            value={props.option.id}
+                            onChange={handleChange}
+                            checked={list[props.questionId] === props.option.id}
+                            name={props.questionId}
+                        />
+                        <label>{props.option.place}</label>
+                    </Grid>
+                    <Grid item xs={10}>
+                        <Typography>{props.option.description}</Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={10}>
-                    <Typography>{props.option.description}</Typography>
-                </Grid>
-            </Grid>
-        </SubCard>
+            </SubCard>
+        </div>
     );
 };
 
