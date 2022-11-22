de87b38afb1cf3decd242e31f981d53fa4278d9b
jarrelp <jarrel.pothoff@gmail.com>
2022-11-22 17:57:20 +0100

    small changes 15315

--- src/layout/MainLayout/index.js
+++ src/layout/MainLayout/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { Outlet } from 'react-router-dom';
 
 // material-ui
@@ -8,7 +8,6 @@ import { AppBar, Box, CssBaseline, Toolbar, useMediaQuery } from '@mui/material'
 // project imports
 import Header from './Header';
 import Sidebar from './Sidebar';
-import navigation from 'menu-items';
 import { drawerWidth } from 'store/constant';
 import { openDrawer } from 'store/slices/menu';
 import { useDispatch, useSelector } from 'store';
@@ -67,8 +66,9 @@ const MainLayout = () => {
     const dispatch = useDispatch();
     const { drawerOpen } = useSelector((state) => state.menu);
 
-    useEffect(() => {
+    React.useEffect(() => {
         dispatch(openDrawer(!matchDownMd));
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [matchDownMd]);
 
     const header = useMemo(
